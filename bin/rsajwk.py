bd189a96e367df1e8d55231f9a5a9b2fd11a2368
Andrew Lush <Andrew.Lush@outlook.com>
2025-04-21 16:34:33 +1000

    Removed Stow

--- bin/rsajwk.py
+++
@@ -1,9 +0,0 @@
-#!/opt/homebrew/bin/python3
-
-import json
-from joserfc.jwk import JWKRegistry
-
-if __name__ == "__main__":
-    key = JWKRegistry.generate_key("RSA", 2048, private=False)
-    print(json.dumps(key.as_dict(), indent=4))
-
